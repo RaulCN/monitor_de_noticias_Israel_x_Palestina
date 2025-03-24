8047150611ac2a27387afa6042d7a3215c1f8830
RaulCN <31525836+RaulCN@users.noreply.github.com>
2025-03-24 01:07:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const url = "https://raw.githubusercontent.com/seu-usuario/monitor-israel-palestina/main/relatorio.txt";
-
+const url = "https://raw.githubusercontent.com/RauICKV/monitor_de_not-cias_Israel_x_Palestina/main/noticias_Israel_palestina/Israel_x_Palestina_23-03-2025_Resumo.txt";
 fetch(url)
   .then(response => response.text())
   .then(data => {
