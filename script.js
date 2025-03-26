2264bb2f49b75f2c0a193381dfba1eeac00df20d
RaulCN <31525836+RaulCN@users.noreply.github.com>
2025-03-26 02:25:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const BASE_RAW_URL = "https://raw.githubusercontent.com/RaulCN/monitor_de_notici
 
 // Lista de relatórios disponíveis (pode ser obtida dinamicamente no futuro)
 const RELATORIOS_DISPONIVEIS = [
-  "24-03-2025",
+  "25-03-2025",
   "24-03-2025",
   "23-03-2025"
 ];
